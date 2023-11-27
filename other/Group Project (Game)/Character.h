3fee39e1e5e51331b4228ef3a054b6c82543c34d
Victor Misyutin <misyutinvictor@gmail.com>
2023-11-26 23:51:35 -0600

    Group Lab final touches

--- other/Group Project (Game)/Character.h
+++ other/Group Project (Game)/Character.h
@@ -15,66 +15,82 @@ public:
     Character();
     // Default Constructor for character
     // post: character is initialized
+
     Character(int a, int l,int s, int h);
     // Overloaded Constructor for character
     // post: character is initialized with a,l,s,h values
+
     void promptAllStats();
     // user is prompted to fill stats of character
     // pre: Character is initialized
     // post: character has stats
+
     int getStatByChar(char c);
     // returns stat using a character to select which stat
     // pre: Character is initialized
     // post: nothing
+
     int getAgility();
     // returns agility
     // pre: Character is initialized
     // post: nothing
+
     int getLuck();
     // returns luck
     // pre: Character is initialized
     // post: nothing
+
     int getStrength();
     // returns strength
     // pre: Character is initialized
     // post: nothing
+
     int getHealth();
     // returns health
     // pre: Character is initialized
     // post: nothing
+
     void loseHealth(int damage);
     // Character loses some health
     // pre: Character is initialized
     // post: damage is subtracted from health
+
     void printStats();
     // prints all character stats
     // pre: Character is initialized
     // post: nothing
+
     void printHealth();
     // prints characters health
     // pre: Character is initialized
     // post: nothing
+
     bool isDead();
     // returns true if character is dead
     // pre: Character is initialized
     // post: nothing
+
     void usePotion();
     // character will use potion and gain 150 health
     // pre: Character is initialized
     // post: if player has any potions left they will gain 150 health
     //       and lose one potion.
+
     void gainPotion();
     // character will gain one potion
     // pre: Character is initialized
     // post: Character will gain a potion
+
     void searchRoom(Room room);
     // character searches room for potions
     // pre: Character and Room are initialized
     // post: Character will gain a potion if they find one
+
     int getNumPotions();
     // returns number of potion that character has
     // pre: Character is initialized
     // post: returns number of potions in players inventory
+    
 private:
     int agility; // stores agility stat
 	int healthStat; // stores characters health stat
--- other/Group Project (Game)/Enemy.h
+++ other/Group Project (Game)/Enemy.h
@@ -13,49 +13,61 @@ public:
     Enemy();
     // Default Constructor for Enemy
     // post: Enemy is initialized
+
     Enemy(int a, int l,int s, int h, string n);
     // Overloaded Constructor for Enemy
     // post: Enemy is initialized with a,l,s,h values
+
     int getAgility();
     // returns agility
     // pre: Enemy is initialized
     // post: nothing
+
     int getLuck();
     // returns luck
     // pre: Enemy is initialized
     // post: nothing
+
     int getStrength();
     // returns strength
     // pre: Enemy is initialized
     // post: nothing
+
     int getHealth();
     // returns health
     // pre: Enemy is initialized
     // post: nothing
+
     void loseHealth(int damage);
     // enemy loses some health
     // pre: Enemy is initialized
     // post: damage is subtracted from health
+
     void printStats();
     // prints all enemy stats
     // pre: Enemy is initialized
     // post: nothing
+
     void printHealth();
     // prints enemys health
     // pre: Enemy is initialized
     // post: nothing
+
     string getName();
     // return name of enemy
     // pre: Enemy is initialized
     // post: nothing
+
     void addAttack(string attk);
     // add attacks to enemies repetoir
     // pre: Enemy is initialized
     // post: attk is added to attack list
+
     bool isDead() const;
     // check if enemy is dead
     // pre: Enemy is initialized
     // post: nothing
+    
 private:
     string name; // stores enemies name
     string attacks[5] = {"", "", "", "", ""}; // stores enemies attacks (maximum of five)
